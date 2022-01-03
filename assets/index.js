40ce79c3bf756386ac392665ef22beb12918c76e
AndresKingterov <tec_grone56@hotmail.com>
2022-01-03 00:59:49 -0500

    segundo commit

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,16 @@
-const video= document.querySelector("video");
-       const button= document.querySelector("button");
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from './plugins/AutoPlay.js';
 
-       function MediaPlayer(config){
-            this.media =config.el 
-          }
+    const video= document.querySelector("video");
 
-       MediaPlayer.prototype.play = function(){
-              this.media.play();
-            }
-       MediaPlayer.prototype.pause = function(){
-              this.media.pause();
-            }     
+    const button= document.querySelector("button");
 
+    const button2= document.querySelector("#MuteOnmute");
 
-       const player = new MediaPlayer({ el: video});  
+    const player = new MediaPlayer({ el: video
+      //, plugins: [ new AutoPlay() ]
+    });  
 
-       button.onclick = () => {
-            if(player.media.paused){
-                player.play();
-            }else{
-              player.pause();
-            }
-       }
+    button.onclick = () => player.togglePlay(); 
+
+    button2.onclick =() => player.mudeonmute(); 
